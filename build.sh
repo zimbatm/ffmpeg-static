a2c4ec49c842500df4830cacc098b03db85651f5
Ian Dennis Miller <ian@iandennismiller.com>
2012-02-01 18:13:22 -0500

    bump zlib to 1.2.6, libogg to 1.3.0, fix libogg on OS X

--- build.sh
+++ build.sh
@@ -16,10 +16,10 @@ mkdir -p "$BUILD_DIR" "$TARGET_DIR"
 echo "#### FFmpeg static build, by STVS SA ####"
 cd $BUILD_DIR
 ../fetchurl "http://www.tortall.net/projects/yasm/releases/yasm-1.0.1.tar.gz"
-../fetchurl "http://zlib.net/zlib-1.2.5.tar.bz2"
+../fetchurl "http://zlib.net/zlib-1.2.6.tar.bz2"
 ../fetchurl "http://www.bzip.org/1.0.5/bzip2-1.0.5.tar.gz"
 ../fetchurl "http://downloads.sourceforge.net/project/libpng/libpng12/1.2.46/libpng-1.2.46.tar.bz2"
-../fetchurl "http://downloads.xiph.org/releases/ogg/libogg-1.2.0.tar.gz"
+../fetchurl "http://downloads.xiph.org/releases/ogg/libogg-1.3.0.tar.gz"
 ../fetchurl "http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.1.tar.bz2"
 ../fetchurl "http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2"
 ../fetchurl "http://webm.googlecode.com/files/libvpx-0.9.1.tar.bz2"
@@ -35,7 +35,7 @@ cd "$BUILD_DIR/yasm-1.0.1"
 make -j 4 && make install
 
 echo "*** Building zlib ***"
-cd "$BUILD_DIR/zlib-1.2.5"
+cd "$BUILD_DIR/zlib-1.2.6"
 ./configure --prefix=$TARGET_DIR
 make -j 4 && make install
 
@@ -51,8 +51,8 @@ make -j 4 && make install
 
 # Ogg before vorbis
 echo "*** Building libogg ***"
-cd "$BUILD_DIR/libogg-1.2.0"
-./configure --prefix=$TARGET_DIR --enable-static --disable-shared
+cd "$BUILD_DIR/libogg-1.3.0"
+./configure --prefix=$TARGET_DIR --build=x86_64 --enable-static --disable-shared
 make -j 4 && make install
 
 # Vorbis before theora
