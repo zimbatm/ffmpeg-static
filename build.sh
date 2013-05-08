5eb526d621f051ea31efb1f37a055281578aa796
Eric Anderson <eric@pixelwareinc.com>
2013-05-08 10:40:00 -0400

    Bump to the latest version of ffmpeg.
    The config patch was removed because examining the configure files seems to
    show that the options are properly added now. I also had no problem building
    ffmpeg once I removed the patch.

--- build.sh
+++ build.sh
@@ -51,7 +51,7 @@ cd $BUILD_DIR
 ../fetchurl "ftp://ftp.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-20120425-2245.tar.bz2"
 ../fetchurl "http://downloads.xvid.org/downloads/xvidcore-1.3.2.tar.gz"
 ../fetchurl "http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz?use_mirror=auto"
-../fetchurl "http://www.ffmpeg.org/releases/ffmpeg-0.10.2.tar.gz"
+../fetchurl "http://www.ffmpeg.org/releases/ffmpeg-1.2.tar.bz2"
 
 echo "*** Building yasm ***"
 cd "$BUILD_DIR/yasm-1.2.0"
@@ -126,8 +126,7 @@ rm -f "$TARGET_DIR/lib/*.so"
 
 # FFMpeg
 echo "*** Building FFmpeg ***"
-cd "$BUILD_DIR/ffmpeg-0.10.2"
-patch -p1 <../../ffmpeg_config.patch
+cd "$BUILD_DIR/ffmpeg-1.2"
 CFLAGS="-I$TARGET_DIR/include" LDFLAGS="-L$TARGET_DIR/lib -lm" ./configure --prefix=${OUTPUT_DIR:-$TARGET_DIR} --extra-version=static --disable-debug --disable-shared --enable-static --extra-cflags=--static --disable-ffplay --disable-ffserver --disable-doc --enable-gpl --enable-pthreads --enable-postproc --enable-gray --enable-runtime-cpudetect --enable-libfaac --enable-libmp3lame --enable-libtheora --enable-libvorbis --enable-libx264 --enable-libxvid --enable-bzlib --enable-zlib --enable-nonfree --enable-version3 --enable-libvpx --disable-devices
 make -j $jval && make install
 
