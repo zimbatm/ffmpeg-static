4303f548beff8efcaa5f727ce073dfba92141603
Eric Anderson <eric@pixelwareinc.com>
2013-05-08 10:42:08 -0400

    Bump zlib
    The version of zlib linked is no longer available. In addition the download
    is now not available in bz2. Change the build script to use the latest.

--- build.sh
+++ build.sh
@@ -40,7 +40,7 @@ mkdir -p "$BUILD_DIR" "$TARGET_DIR"
 echo "#### FFmpeg static build, by STVS SA ####"
 cd $BUILD_DIR
 ../fetchurl "http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz"
-../fetchurl "http://zlib.net/zlib-1.2.7.tar.bz2"
+../fetchurl "http://zlib.net/zlib-1.2.8.tar.gz"
 ../fetchurl "http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz"
 ../fetchurl "ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng12/libpng-1.2.50.tar.gz"
 ../fetchurl "http://downloads.xiph.org/releases/ogg/libogg-1.3.0.tar.gz"
@@ -59,7 +59,7 @@ cd "$BUILD_DIR/yasm-1.2.0"
 make -j $jval && make install
 
 echo "*** Building zlib ***"
-cd "$BUILD_DIR/zlib-1.2.7"
+cd "$BUILD_DIR/zlib-1.2.8"
 ./configure --prefix=$TARGET_DIR
 make -j $jval && make install
 
